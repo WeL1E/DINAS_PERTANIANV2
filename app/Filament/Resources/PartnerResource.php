d2bdb5f3bf3289af73698b4177b317423c70ca90
WeL1E <aldoripaldo73@gmail.com>
2024-07-17 02:20:53 +0700

    aldo

---
+++ app/Filament/Resources/PartnerResource.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Partner;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use App\Filament\Resources\PartnerResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\PartnerResource\RelationManagers;
+
+class PartnerResource extends Resource
+{
+    protected static ?string $model = Partner::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    Forms\Components\TextInput::make('title')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\FileUpload::make('thumbnail')
+                        ->required()->image()->disk('public')
+                        ->maxSize(1024),
+                    Forms\Components\RichEditor::make('content')
+                        ->required()
+                        ->columnSpanFull(),
+                    Forms\Components\TextInput::make('link')
+                        ->required()
+                        ->maxLength(255),
+                ])
+            ]);
+    }
+
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable()->sortable(),
+                Tables\Columns\ImageColumn::make('thumbnail')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('link')
+                    ->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make()->after(function (Collection
+                $records) {
+                    foreach ($records as $key => $value) {
+                        if ($value->thumbnail) {
+                            Storage::disk('public')->delete($value->thumbnail);
+                        }
+                    }
+                }),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPartners::route('/'),
+            'create' => Pages\CreatePartner::route('/create'),
+            'edit' => Pages\EditPartner::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/SectionResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Section;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Illuminate\Support\Facades\Storage;
+use Filament\Forms\Components\Component;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use App\Filament\Resources\SectionResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\SectionResource\RelationManagers;
+
+class SectionResource extends Resource
+{
+    protected static ?string $model = Section::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    Forms\Components\TextInput::make('title')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\FileUpload::make('thumbnail')
+                        ->required()->image()->disk('public')
+                        ->maxSize(1024),
+                    Forms\Components\RichEditor::make('content')
+                        ->required()
+                        ->columnSpanFull(),
+                    Forms\Components\select::make('post_as')->options([
+                        'JUMBOTRON' => 'JUMBOTRON',
+                        'ABOUT' => 'ABOUT'
+                    ])
+                    ->required()
+                ]),
+            ]);
+    }
+
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable()->sortable(),
+                Tables\Columns\ImageColumn::make('thumbnail')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('post_as')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make()->after(function (Collection
+                $records) {
+                    foreach ($records as $key => $value) {
+                        if ($value->thumbnail) {
+                            Storage::disk('public')->delete($value->thumbnail);
+                        }
+                    }
+                }),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSections::route('/'),
+            'create' => Pages\CreateSection::route('/create'),
+            'edit' => Pages\EditSection::route('/{record}/edit'),
+        ];
+    }
+}
