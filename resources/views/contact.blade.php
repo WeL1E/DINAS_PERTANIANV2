d556454ee8172bf909cc3ff537283b9fb118a1d0
WeL1E <aldoripaldo73@gmail.com>
2024-07-18 08:17:03 +0700

    albi

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,10 @@
+@php
+$site_name = get_setting_value('_site_name');
+$site_desc = get_setting_data('_site_description');
+$git = get_setting_dataG('_GitHub');
+$loc = get_setting_dataL('_location');
+@endphp
+
 <!DOCTYPE html>
 <html>
 
@@ -8,26 +15,26 @@
   <!-- Mobile Metas -->
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <!-- Site Metas -->
-  <link rel="icon" href="images/fevicon.png" type="image/gif" />
+  <link rel="icon" href="{{ asset('storage/home-page') }}/assets/images/favicon1.png" type="image/gif" />
   <meta name="keywords" content="" />
   <meta name="description" content="" />
   <meta name="author" content="" />
 
-  <title>Edgecut</title>
+  <title>{{ $site_name }}</title>
 
 
   <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('storage/home-page') }}/assets/css/bootstrap.css" />
 
   <!-- fonts style -->
   <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet" />
 
   <!-- font awesome style -->
-  <link href="css/font-awesome.min.css" rel="stylesheet" />
+  <link href="{{ asset('storage/home-page') }}/assets/css/font-awesome.min.css" rel="stylesheet" />
   <!-- Custom styles for this template -->
-  <link href="css/style.css" rel="stylesheet" />
+  <link href="{{ asset('storage/home-page') }}/assets/css/style.css" rel="stylesheet" />
   <!-- responsive style -->
-  <link href="css/responsive.css" rel="stylesheet" />
+  <link href="{{ asset('storage/home-page') }}/assets/css/responsive.css" rel="stylesheet" />
 
 </head>
 
@@ -39,7 +46,7 @@
       <nav class="navbar navbar-expand-lg custom_nav-container ">
         <a class="navbar-brand" href="index.html">
           <span>
-            Edgecut
+            {{ $site_name }}
           </span>
         </a>
         </a>
@@ -49,36 +56,30 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
-            <ul class="navbar-nav ">
-              <li class="nav-item ">
-                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.html"> About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="furniture.html">Furnitures</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="blog.html">Blog</a>
-              </li>
+            <ul class="navbar-nav  ">
               <li class="nav-item active">
-                <a class="nav-link" href="contact.html">Contact Us</a>
+                <a class="nav-link" href="{{ url('/') }}">Beranda <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ url('/about') }}">Tentang</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ url('/contact') }}">Hubungi!</a>
               </li>
             </ul>
           </div>
           <div class="quote_btn-container">
-            <a href="">
+            <a href="{{ url('/admin') }}">
               <span>
                 Login
               </span>
               <i class="fa fa-user" aria-hidden="true"></i>
             </a>
-            <form class="form-inline">
+            {{-- <form class="form-inline">
               <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
                 <i class="fa fa-search" aria-hidden="true"></i>
               </button>
-            </form>
+            </form> --}}
           </div>
         </div>
       </nav>
@@ -87,35 +88,17 @@
   </div>
 
   <!-- contact section -->
-  <section class="contact_section  long_section">
+  {{-- <section class="contact_section  long_section">
     <div class="container">
       <div class="row">
         <div class="col-md-6">
           <div class="form_container">
             <div class="heading_container">
               <h2>
-                Contact Us
+                Alamat
               </h2>
             </div>
-            <form action="">
-              <div>
-                <input type="text" placeholder="Your Name" />
-              </div>
-              <div>
-                <input type="text" placeholder="Phone Number" />
-              </div>
-              <div>
-                <input type="email" placeholder="Email" />
-              </div>
-              <div>
-                <input type="text" class="message-box" placeholder="Message" />
-              </div>
-              <div class="btn_box">
-                <button>
-                  SEND
-                </button>
-              </div>
-            </form>
+            {{ $loc }}
           </div>
         </div>
         <div class="col-md-6">
@@ -127,7 +110,7 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> --}}
   <!-- end contact section -->
 
   <!-- info section -->
@@ -135,27 +118,27 @@
 
     <div class="container">
       <div class="contact_nav">
-        <a href="">
+        <a href="https://github.com/WeL1E/DINAS_PERTANIANV2">
           <i class="fa fa-phone" aria-hidden="true"></i>
           <span>
-            Call : +01 123455678990
+            GitHub  : {{ $git }}
           </span>
         </a>
         <a href="">
           <i class="fa fa-envelope" aria-hidden="true"></i>
           <span>
-            Email : demo@gmail.com
+            Email : wefarm356547@gmaail.com
           </span>
         </a>
         <a href="">
           <i class="fa fa-map-marker" aria-hidden="true"></i>
           <span>
-            Location
+            {{ $loc }}
           </span>
         </a>
       </div>
 
-      <div class="info_top ">
+      {{-- <div class="info_top ">
         <div class="row ">
           <div class="col-sm-6 col-md-4 col-lg-3">
             <div class="info_links">
@@ -178,22 +161,22 @@
               </h5>
               <div class="post_box">
                 <div class="img-box">
-                  <img src="images/f1.png" alt="">
+                  <img src="{{ asset('storage/home-page') }}/assets/images/f1.png" alt="">
                 </div>
                 <div class="img-box">
-                  <img src="images/f2.png" alt="">
+                  <img src="{{ asset('storage/home-page') }}/assets/images/f2.png" alt="">
                 </div>
                 <div class="img-box">
-                  <img src="images/f3.png" alt="">
+                  <img src="{{ asset('storage/home-page') }}/assets/images/f3.png" alt="">
                 </div>
                 <div class="img-box">
-                  <img src="images/f4.png" alt="">
+                  <img src="{{ asset('storage/home-page') }}/assets/images/f4.png" alt="">
                 </div>
                 <div class="img-box">
-                  <img src="images/f5.png" alt="">
+                  <img src="{{ asset('storage/home-page') }}/assets/images/f5.png" alt="">
                 </div>
                 <div class="img-box">
-                  <img src="images/f6.png" alt="">
+                  <img src="{{ asset('storage/home-page') }}/assets/images/f6.png" alt="">
                 </div>
               </div>
             </div>
@@ -227,7 +210,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> --}}
   </section>
   <!-- end info_section -->
 
